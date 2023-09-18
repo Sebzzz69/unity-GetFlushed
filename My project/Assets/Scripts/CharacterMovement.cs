b04dbb38b3be2ae8fe1173c6607d3a9c87ce635e
Sebzzz69 <callenholmljungsebastian@gmail.com>
2023-09-18 21:35:19 +0200

    updating score works fine

--- My project/Assets/Scripts/CharacterMovement.cs
+++ My project/Assets/Scripts/CharacterMovement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditor;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
@@ -9,24 +11,55 @@ public class CharacterMovement : MonoBehaviour
 
     private Vector2 velocity;
 
+    [SerializeField] private KeyCode keyUp;
+    [SerializeField] private KeyCode keyLeft;
+    [SerializeField] private KeyCode keyDown;
+    [SerializeField] private KeyCode keyRight;
+
+    float horizontalInput;
+    float verticalInput;
+
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        CheckInput();
 
         Vector2 movementDirection = new Vector2(horizontalInput, verticalInput).normalized;
 
-        // Calculate velocity based on input and speed
+        // Calculate velocity based on input and speed 
         velocity = movementDirection * speed;
 
         // Move the character
         Vector2 newPosition = (Vector2)transform.position + velocity * Time.deltaTime;
         transform.position = newPosition;
 
+        horizontalInput = 0;
+        verticalInput = 0;
+
         // Apply damping to gradually stop the character
         velocity = Vector2.Lerp(velocity, Vector2.zero, damping * Time.deltaTime);
 
 
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKey(keyUp))
+        {
+            verticalInput = 1;
+        }
+        else if (Input.GetKey(keyDown))
+        {
+            verticalInput = -1;
+        }
+
+        if (Input.GetKey(keyRight))
+        {
+            horizontalInput = 1;
+        }
+        else if (Input.GetKey(keyLeft))
+        {
+            horizontalInput = -1;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
---
+++ My project/Assets/Scripts/HandleScore.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HandleScore : MonoBehaviour
+{
+    [SerializeField] TMP_Text textScore;
+
+    int score;
+
+    public void UpdateScore()
+    {
+        score++;
+        textScore.text = score.ToString();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            UpdateScore();
+        }
+    }
+}
